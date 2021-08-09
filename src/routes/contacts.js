a7f84108f6d572adf8cb88f17d6d80b3b08bba6d
Bark Sanson <barkSansonDev@gmail.com>
2021-08-10 01:24:40 +0200

    Added add, editing and show contacts

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,8 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
-    res.send('Showing all contacts');
-})
+const Contact = require("../models/contact");
+
+router.get("/", async (req, res) => {
+  const contacts = await Contact.find().lean();
+  res.render("contacts/all-contacts", { contacts });
+});
+
+router.get("/add", (req, res) => {
+  res.render("contacts/add-contact");
+});
+
+router.post(
+  "/add",
+  body("name").notEmpty().withMessage("Name field can't be empty!"),
+  body("phone").notEmpty().withMessage("Phone field can't be empty!"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("contacts/add-contact", errors);
+    }
+
+    const { name, surname, email, phone } = req.body;
+    const contact = new Contact({ name, surname, email, phone });
+
+    await contact.save();
+    res.redirect("/contacts");
+  }
+);
+
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const contact = await Contact.findById(id).lean();
+
+  res.render("contacts/edit-contact", { contact });
+});
+
+router.put(
+  "/edit/:id",
+  body("name").notEmpty().withMessage("Name field can't be empty!"),
+  body("phone").notEmpty().withMessage("Phone field can't be empty!"),
+  async (req, res) => {
+    const contact = await Contact.findById(req.params.id).lean();
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render("contacts/edit-contact", { errors, contact });
+    }
+
+    const { name, surname, email, phone } = req.body;
+    await Contact.findByIdAndUpdate(req.params.id, { name, surname, email, phone }, { useFindAndModify: false});
+    res.redirect('/contacts');
+  } 
+);
 
 module.exports = router;
